50ed4ec42faa484a6cbd5fce65b003c27be291f3
Shreyanshi Shukla <shreyanshi.shukla@nagarro.com>
2023-02-21 09:01:42 +0530

    project commit

---
+++ src/main/java/com/azurespring/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.azurespring.controller;
+
+import java.util.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import entity.Employee;
+
+@RestController
+public class HomeController {
+	List<Employee> lst =new ArrayList<>();
+	//lst.add(new Employee(1,"abc","IT",500000));
+	@GetMapping("/workers")
+	public List<Employee> greeting() {
+		/*lst.add(new Employee(1,"abc","IT",500000));
+		lst.add(new Employee(2,"ranju","QA",800000));
+		lst.add(new Employee(3,"rani","Business",600000));
+		lst.add(new Employee(4,"raja","Software",1000000));
+   
+    */
+  
+		return lst;
+	}
+
+}
